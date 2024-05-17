afb609524360d382c913d125e53693493f92f01e
Brent Junir Seabelo Tshekane <160117169+Brent-T@users.noreply.github.com>
2024-05-17 17:01:46 +0200

    Add files via upload

--- Javascript/Warner Bros.js
+++ Javascript/Warner Bros.js
@@ -1,1 +1,19 @@
+// Get all video elements
+var videos = document.querySelectorAll('.item video');
+
+// Define function to play video on hover
+function playVideo(event) {
+    event.target.play();
+}
+
+// Define function to pause video on mouse leave
+function pauseVideo(event) {
+    event.target.pause();
+}
+
+// Add event listeners to each video
+videos.forEach(function(video) {
+    video.addEventListener('mouseover', playVideo);
+    video.addEventListener('mouseout', pauseVideo);
+});
 
